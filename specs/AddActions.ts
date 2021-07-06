bdcbdd519ae6ef9076c22ed497f017b42a7f5d15
martinberlin2 <70579601+martinberlin2@users.noreply.github.com>
2021-07-06 19:41:52 +0200

    kleine Änderungen.-Wie ha es Jan gemacht?

---
+++ specs/AddActions.ts
@@ -0,0 +1,34 @@
+// import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
+import { browser, element, by} from 'protractor';
+
+// AddActions.ts
+import {AngularHomepage} from '../pageobjects/angularHomepagePO';
+const angHomepage = new AngularHomepage();
+
+//describe('angularjs homepage', () => {
+describe('AddActions', async () => {
+ 
+  beforeEach(async () => {
+    await angHomepage.load();
+  });
+
+  it('Tests From csv', async () => {
+	angular.forEach
+		
+  });
+		
+		
+		
+	  
+    await element(by.model('yourName')).sendKeys('Julie');
+    const greeting = element(by.binding('yourName'));
+    expect(await greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  it('should list todos', async () => {
+    const todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(await todoList.count()).toEqual(2);
+    expect(await todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+})
+
